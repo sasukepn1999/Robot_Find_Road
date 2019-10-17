0331b0f8d0b51edf271896319239524c79d20460
sasukepn1999 <36978783+sasukepn1999@users.noreply.github.com>
2019-10-17 12:08:43 +0700

    Update main.py
    Add options about GUI (turtle or pygame).

--- src/main.py
+++ src/main.py
@@ -1,14 +1,14 @@
 import sys
-#import turtle as tt
+import turtle as tt
 from find_way import dac_find_path
 from find_way import dijkstra_find_path
 from find_way import astar_find_path
 import os
-#import GUI
+import GUI
 import GUI_V2 as gv2
 
 def main():
-    if (len(sys.argv) == 3 and (sys.argv[2] == '0' or sys.argv[2] == '1' or sys.argv[2] == '2')):
+    if (len(sys.argv) == 4 and (sys.argv[2] == '0' or sys.argv[2] == '1' or sys.argv[2] == '2')):
         input_filepath = sys.argv[1]
 
         if not (os.path.exists(input_filepath)):
@@ -32,22 +32,24 @@ def main():
             m_find_path.update_map_mat()
             matrix = m_find_path.map_mat
 
-        # #test
-        # path1 = path[1]
+        if sys.argv[3] == "turtle":
+            path1 = path[1]
+            screen = tt.Screen()
+            Long = tt.Turtle()
+            Long.hideturtle()
+            turtle = Long
+            tt.tracer(0, 0)
 
-        # Long = tt.Turtle()
-        # Long.hideturtle()
-        # turtle = Long
-
-        # GUI.grid(turtle, len(matrix), len(matrix[0]))
-        # GUI.fillColorOneElementOnGridByMatrix(turtle, matrix, GUI.LENGTH)
-        # GUI.findWay(turtle, path1, GUI.LENGTH)
-
-        # screen.exitonclick()
-        surface = gv2.init(matrix)
-        gv2.loop(surface, matrix, path)
+            GUI.grid(turtle, len(matrix), len(matrix[0]))
+            GUI.fillColorOneElementOnGridByMatrix(turtle, matrix, GUI.LENGTH)
+            GUI.findWay(turtle, path1, GUI.LENGTH)
+            
+            screen.exitonclick()
+        elif sys.argv[3] == "pygame":
+            surface = gv2.init(matrix)
+            gv2.loop(surface, matrix, path)
     else:
-        print("Usage:", sys.argv[0], 'input_filepath', 'type_algo_(0-2)')
+        print("Usage:", sys.argv[0], 'input_filepath', 'type_algo(0-2)', 'type_gui(turtle or pygame)')
 
 
 if __name__ == '__main__':
