c7c8b3d42c1a59214d075aeef40036b040a800d9
nhatanhst <38091700+nhatanhst@users.noreply.github.com>
2019-10-19 13:39:37 +0700

    Add files via upload

--- src/main.py
+++ src/main.py
@@ -6,6 +6,7 @@ from find_way import astar_find_path
 import os
 import GUI
 import GUI_V2 as gv2
+import time
 
 
 def poly_moving(m_find_path, turtle):
@@ -32,25 +33,61 @@ def main():
         find_algo = sys.argv[2]
         if find_algo == '0':
             m_find_path = dijkstra_find_path.Dijkstra_Find_Path(input_filepath)
+            time_start = time.time()
             path = m_find_path.get_path()
+            time_end = time.time()
             m_find_path.update_map_mat()
             matrix = m_find_path.map_mat
         if find_algo == '1':
             m_find_path = astar_find_path.AStar_Find_Path(input_filepath)
+            time_start = time.time()
             path = m_find_path.get_path()
+            time_end = time.time()
             m_find_path.update_map_mat()
             matrix = m_find_path.map_mat
         if find_algo == '2':
             m_find_path = dac_find_path.Dac_Find_Path(input_filepath)
+            time_start = time.time()
             path = m_find_path.get_path()
+            time_end = time.time()
             m_find_path.update_map_mat()
             matrix = m_find_path.map_mat
 
         if find_algo == "3":
-            m_find_path = dac_find_path.Dac_Find_Path(input_filepath)
-            m_find_path.update_map_mat()
-            path = (0, [])
-            matrix = m_find_path.map_mat
+            #m_find_path = dac_find_path.Dac_Find_Path(input_filepath)
+            #m_find_path.update_map_mat()
+            #path = (0, [])
+            #matrix = m_find_path.map_mat
+
+            screen = tt.Screen()
+            Long = tt.Turtle()
+            Long.hideturtle()
+            turtle = Long
+            tt.tracer(0, 0)
+
+            m_find_path = astar_find_path.AStar_Find_Path(input_filepath)
+
+            path = []
+            while 1:
+                matrix = m_find_path.get_matrix()
+                check, tmp_path = m_find_path.move()
+                path = path + tmp_path
+                #print(check)
+
+                GUI.grid(turtle, len(matrix), len(matrix[0]))
+                GUI.fillColorOneElementOnGridByMatrix(turtle, matrix, GUI.LENGTH)
+                GUI.findWay(turtle, path, matrix, GUI.LENGTH)
+                #break
+                if check == True:
+                    break
+
+            screen.exitonclick()
+
+
+        #print(time_end - time_start)
+
+        if find_algo == '3':
+            return
 
         if sys.argv[3] == "turtle":
             path1 = path[1]
@@ -62,7 +99,7 @@ def main():
 
             GUI.grid(turtle, len(matrix), len(matrix[0]))
             GUI.fillColorOneElementOnGridByMatrix(turtle, matrix, GUI.LENGTH)
-            GUI.findWay(turtle, path1, GUI.LENGTH)
+            GUI.findWay(turtle, path1, matrix, GUI.LENGTH)
 
             screen.exitonclick()
 
